6c5457b37cea263ce3f0008a026d1b0d6b721bdd
mihawk <mihawk@vikvon.com>
2015-04-24 16:10:23 +0800

    add start boss db in tpl

--- priv/naga/src/project_app.erl
+++ priv/naga/src/project_app.erl
@@ -4,12 +4,10 @@
 -export([start/2]).
 -export([stop/1]).
 
-
 -spec start(_, _) -> {ok, pid()}.
 start(_, _) ->
-    io:format("starting {{appid}}...~n"),
-    application:start(naga),
-    naga:start({{appid}}),
+    error_logger:info_msg("Starting application {{appid}}...", []),
+    {{appid}}:start_db(),
     {{appid}}_sup:start_link().
 
 -spec stop(_) -> ok.
