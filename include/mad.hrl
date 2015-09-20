60d1935a4196c725fc66b265f6689806130a72a6
Namdak Tonpa <maxim@synrc.com>
2015-09-21 01:14:31 +0300

    cleanup output
    Conflicts:
    	include/mad.hrl
    	mad

--- include/mad.hrl
+++ include/mad.hrl
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 -define(VERSION,"3c9f9c").
 =======
 -define(VERSION,"de49b2").
 >>>>>>> 26a29f5... exclude fs and active from synrc mad releases
+=======
+-define(VERSION,"26a29f").
+>>>>>>> bb0231a... cleanup output
