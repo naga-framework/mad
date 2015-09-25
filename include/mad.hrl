34283c37198012e54d8217d26aeef3075defe981
Namdak Tonpa <maxim@synrc.com>
2015-09-25 14:20:59 +0300

    mad rel means script bundle
    Conflicts:
    	include/mad.hrl
    	mad
    	src/packaging/mad_release.erl

--- include/mad.hrl
+++ include/mad.hrl
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 -define(VERSION,"c7dd54").
 =======
 -define(VERSION,"ed24a9").
 >>>>>>> cd4b593... fix script release
+=======
+-define(VERSION,"cd4b59").
+>>>>>>> 2405ebf... mad rel means script bundle
