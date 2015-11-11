8d43a5c41a54dcd43a0e84a4a1f16e46280c9279
mihawk <chan.sisowath@gmail.com>
2015-11-11 15:18:04 +0800

    update tpl

--- priv/hello/project_index.erl
+++
@@ -1,8 +0,0 @@
--module(index).
--compile(export_all).
--include_lib("n2o/include/wf.hrl").
--include_lib("nitro/include/nitro.hrl").
-
-hello(<<"GET">>, _, _)   -> {ok, [{msg, "Hello World!!!!!"}]}.
-
-event(Event) -> wf:info(?MODULE,"Unknown Event: ~p~n",[Event]).
