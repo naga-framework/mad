71a0a37991fffc43d5dc64d9e3ad795973712024
mihawk <chan.sisowath@gmail.com>
2016-12-23 09:50:25 +0700

    add common_test

--- src/provision/mad_repl.erl
+++ src/provision/mad_repl.erl
@@ -4,7 +4,7 @@
 
 disabled() -> [].
 system() -> [compiler,syntax_tools,sasl,tools,mnesia,reltool,xmerl,crypto,kernel,stdlib,ssh,eldap,erl_interface,
-             wx,ssl,runtime_tools,public_key,observer,inets,asn1,et,eunit,hipe,os_mon,parsetools,odbc].
+             wx,ssl,runtime_tools,public_key,observer,inets,asn1,et,eunit,hipe,os_mon,parsetools,odbc,common_test].
 
 local_app() ->
     case filename:basename(filelib:wildcard("ebin/*.app"),".app") of
